962f281a997be415fee217645b9a8ef995f92edd
Kyle Zaragoza <popwarsweet@gmail.com>
2016-09-19 10:04:09 +0700

    adds 0.5 second delay to blur animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 window.onload = function () {
-    // remove blur once page has loaded
-    document.getElementsByTagName("html")[0].classList.add('blur-animation');
+    // wait 500ms before removing blur
     setTimeout(function () {
-        document.getElementsByTagName("html")[0].classList.remove('blur');
-        document.getElementsByTagName("html")[0].classList.remove('blur-animation');
-    }, 1200);
+        // add blur animation
+        document.getElementsByTagName("html")[0].classList.add('blur-animation');
+        setTimeout(function () {
+            // remove blur class after animation
+            // NOTE: we're removing this class instead of settings `forwards` on animation because Safari renders text poorly w/ `blur(0px)` applied.
+            document.getElementsByTagName("html")[0].classList.remove('blur');
+            document.getElementsByTagName("html")[0].classList.remove('blur-animation');
+        }, 1200);
+    }, 500);
 }
